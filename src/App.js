c473356e97d332989de5de802205d573e7b3d61a
Alex Madrigal <jam.madrigal@gmail.com>
2020-11-02 11:12:41 -0800

    added some pseudocode more specifically

--- src/App.js
+++ src/App.js
@@ -4,10 +4,20 @@ import TableHead from './components/TableHeader/TableHeader'
 
 
 function App() {
+  // Make this to a state instead for the filtering to function
+  const employees = [{}];
+
+  // API call here that pushes to employees
   return (
-    // There also needs to be a top page banner here with the title of the app
+
+    // There needs to be a top page banner here with the title of the app
+
     // Also add a search bar component, which filters the table as the user types
-    // Create clickable buttons on the table headres that sort the table by that header, either ascending or descending on click
+    // Listens whenever there is a change on the input and searching for whatever matches the input in the object, use state to remove everything that doesn't match (activity 17)
+
+    // Create clickable buttons on the table headers that sort the table by that header, either ascending or descending on click 
+
+    // Add object for an api call here, push the object to an array, map over it and 
     <div className="container">
 
       <div className="row">
@@ -15,10 +25,9 @@ function App() {
         <table className="col-sm" style={{margin: '25px 0', 'font-size': '0.9em', 'min-width': '400px', 'border-radius': '10px 10px 0 0', overflow: 'hidden', 'box-shadow': '0 0 20px rgba(0, 0, 0, 0.15)'}}>
         <TableHead />
           <tbody>
-            <Row />
-            <Row />
-            <Row />
-            <Row />
+            {employees.map((employee) => <Row picture={employee.picture}/>)}
+
+
           </tbody>
         </table>
         
--- src/components/Row/Row.js
+++ src/components/Row/Row.js
@@ -5,12 +5,11 @@ import API from '../../utils/API';
 function Row(props) {
   // Add an API.get from here that gets the employees json file from the API.getUsers and stores it in the employees variable below (?)
   // useState and useEffect?
-  const employees = [{}]
 
   return (
     // Add a map() here that iterates through the array of employees and generates a row with the props in their corresponding columns
     <tr>
-        <td>Imagehere Firstnamehere Lastnamehere</td>
+        <td>{props.picture} Firstnamehere Lastnamehere</td>
         <td>Genderhere</td>
         <td>Cityhere Statehere</td>
         <td>Emailhere</td>
